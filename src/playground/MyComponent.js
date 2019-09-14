ca8fdceb05d062954e8cbeb6599fac07f96d3db7
Rafael Garcia <dev.ragare@gmail.com>
2019-09-14 09:44:19 +0200

    Some more things

--- src/playground/MyComponent.js
+++ src/playground/MyComponent.js
@@ -6,15 +6,30 @@ import { Button, Intent, Spinner } from '@blueprintjs/core'
 const MyComponent = () => {
     return (
         <div>
-            <p>This is mycomponent</p>
+            <h1>This is mycomponent</h1>
+            <h2>Subtitle</h2>
             <Spinner intent={Intent.PRIMARY} />
-            <div className="play1">
+            <div>
                 More than a decade ago, we set out to create products that would transform
                 the way organizations use their data. Today, our products are deployed at
                 the most critical government, commercial, and non-profit institutions in
                 the world to solve problems we hadn’t even dreamed of back then.
             </div>
+            <div className="bp3-blockquote">This is a block that I want to see</div>
+            <ol className=".modifier">
+                <li>Item the first</li>
+                <li>Item the second</li>
+                <li>
+                    Item the third
+                    <ul className=".modifier">
+                        <li>Item the fourth</li>
+                        <li>Item the fifth</li>
+                    </ul>
+                </li>
+            </ol>
+            <Button text="I'm a button" />
         </div>
+
 
     )
 }
