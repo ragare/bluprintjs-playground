633cbe129551e410f179dd05d47b7c0588378de5
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-09-17 19:04:04 +0200

    More table things

--- src/playground/PlayTable.js
+++ src/playground/PlayTable.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { H5 } from '@blueprintjs/core'
-import { Cell, Column, Table } from "@blueprintjs/table"
+import { Cell, Column, Table, ColumnHeaderCell } from "@blueprintjs/table"
 
 const PlayTable = () => {
-    const cellRenderer = (rowIndex) => {
+    const cellRenderer = (rowIndex, columnIndex) => {
+        //console.log('[1] ROW %s COLUMN %s', rowIndex, columnIndex)
         return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>
     };
+    const cellRenderer2 = (rowIndex, columnIndex) => {
+        //console.log('[2] ROW %s COLUMN %s', rowIndex, columnIndex)
+        return <Cell tooltip={`Yo soy ${rowIndex}`}>{`NAME ${rowIndex}`}</Cell>
+    };
+    const TestRender = () => {
+        console.log("test render")
+        return <ColumnHeaderCell name={'Yo que se'}/>
+    }
     return (
         <>
             <H5>This will be a table</H5>
-            <Table numRows={10}>
-                <Column name="Dollars" cellRenderer={cellRenderer} />
-            </Table>
+            <div style={{ height: "500px" }}>
+                <Table numRows={10}
+                    enableColumnReordering={true}
+                    enableColumnResizing={false}
+                    enableRowReordering={true}
+                    enableRowResizing={false}
+                    enableGhostCells={true}>
+                    <Column name="Dollars" cellRenderer={cellRenderer} columnHeaderCellRenderer={TestRender}/>
+                    <Column name="Names" cellRenderer={cellRenderer2} />
+                </Table>
+            </div>
         </>
     )
 }
