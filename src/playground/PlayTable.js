dabcb171b4dff7f1fe66311968044c2d9efa6164
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-09-18 11:50:55 +0200

    Table with prog data

--- src/playground/PlayTable.js
+++ src/playground/PlayTable.js
@@ -1,36 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { H5, Menu, MenuItem } from '@blueprintjs/core'
 import { Cell, Column, Table, ColumnHeaderCell } from "@blueprintjs/table"
 
 const PlayTable = () => {
+    const [tableData, setTableData] = useState([
+        {
+            id: 'XDFATTA',
+            name: 'John Viconst',
+            birth: '20190202',
+            age: 33,
+            savings: 123.26
+        },
+        {
+            id: 'GRER899',
+            name: 'Mary Shelly',
+            birth: '18920203',
+            age: 87,
+            savings: 2600.00
+        },
+        {
+            id: 'KLS67YUS',
+            name: 'Boris Acoont',
+            birth: '19330506',
+            age: 77,
+            savings: 0.0
+        },
+        {
+            id: '56GTH777',
+            name: 'Lea Volker',
+            birth: '19980101',
+            age: 45,
+            savings: 1203.07
+        }
+    ])
+
+    const columns = [
+        { name: 'ID', field: 'id'},
+        { name: 'NAME', field: 'name'},
+        { name: 'BIRTH', field: 'birth'},
+        { name: 'AGE', field: 'age'},
+        { name: 'SAVINGS', field: 'savings'},
+    ]
+
+
     const cellRenderer = (rowIndex, columnIndex) => {
         //console.log('[1] ROW %s COLUMN %s', rowIndex, columnIndex)
-        return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>
+        console.log('celldata', tableData[rowIndex])
+        return <Cell>{tableData[rowIndex][columns[columnIndex]['field']]}</Cell>
     };
-    const cellRenderer2 = (rowIndex, columnIndex) => {
-        //console.log('[2] ROW %s COLUMN %s', rowIndex, columnIndex)
-        return <Cell tooltip={`Yo soy ${rowIndex}`}>{`NAME ${rowIndex}`}</Cell>
-    };
-    const TestRender = () => {
-        console.log("test render")
-        return <ColumnHeaderCell name={'Yo que se'} menuRenderer={MenuRenderer} />
+    const columnRender = (columnIndex) => {
+        //console.log('[TEST] COLUMN %s', columnIndex)
+        return <ColumnHeaderCell name={columns[columnIndex]['name']} menuRenderer={MenuRenderer} />
+    }
+    const sortAsc = () => {
+        console.log("Sort ASC")
+    }
+    const sortDesc = () => {
+        console.log("Sort DESC")
     }
     const MenuRenderer = () => {
         return (
             <Menu>
-                <MenuItem text="Child one" />
-                <MenuItem text="Child two" />
-                <MenuItem text="Child three" />
+                <MenuItem icon="sort-asc" onClick={sortAsc} text="Sort Asc" />
+                <MenuItem icon="sort-desc" onClick={sortDesc} text="Sort Desc" />
             </Menu>
         )
+    }
+    const columnsReordered = () => {
+        console.log('Columns reordered')
     }
     return (
         <>
             <H5>This will be a table</H5>
             <div style={{ height: "500px" }}>
-                <Table numRows={10}>
-                    <Column name="Dollars" cellRenderer={cellRenderer} columnHeaderCellRenderer={TestRender} />
-                    <Column name="Names" cellRenderer={cellRenderer2} />
+                <Table numRows={tableData.length}
+                    enableColumnReordering={true}
+                    onColumnsReordered={columnsReordered}
+                >
+                    {
+                        columns.map((c) => {
+                            return <Column columnHeaderCellRenderer={columnRender} cellRenderer={cellRenderer}  />
+                        })
+                    }
+                    {/* <Column name="Dollars" cellRenderer={cellRenderer} columnHeaderCellRenderer={columnRender} />
+                    <Column name="Names" cellRenderer={cellRenderer2} /> */}
                 </Table>
             </div>
         </>
